d3bdd35c2da3ba5c67e689896144ce2a6b70fe49
Henrique Tavares <ihenrits@gmail.com>
2020-02-29 21:02:10 -0300

    feat: creating style layout

---
+++ src/pages/_layouts/auth/styles.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: linear-gradient(-90deg, #7159c1, #ab59c1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 315px;
+  text-align: center;
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+    input {
+      border: 1px solid #ccc;
+      border-radius: 10px;
+      height: 44px;
+      padding: 0 15px;
+      color: #5b5b5b;
+      margin: 0 0 10px;
+      &::placeholder {
+        color: #5b5b5b;
+      }
+    }
+    span {
+      color: #fb6f91;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
+    }
+    label {
+      color: #5b5b5b;
+      font-weight: 600;
+    }
+    button {
+      margin: 5px 0 0;
+      height: 44px;
+      background: #816fe7 !important;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
+      &:hover {
+        background: ${darken(0.03, '#3b9eff')};
+      }
+    }
+    a {
+      color: #fff;
+      margin-top: 15px;
+      font-size: 16px;
+      opacity: 0.8;
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+`;
