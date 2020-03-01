713c803db631c4ba0119528d51d6fdd9468a7584
Henrique Tavares <ihenrits@gmail.com>
2020-02-29 21:00:31 -0300

    feat: creating SignIn page

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,85 @@
+import React, { useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as Yup from 'yup';
+
+import { Form } from '@unform/web';
+import { BounceLoader } from 'react-spinners';
+
+import { signInRequest } from '~/store/modules/auth/actions';
+
+import Input from '~/components/Input';
+
+import { Container, Button } from './styles';
+
+import logo from '~/assets/fastfeet-logo.png';
+
+export default function SignIn() {
+  const formRef = useRef(null);
+
+  const dispatch = useDispatch();
+
+  const loading = useSelector(state => state.auth.loading);
+
+  async function handleSubmit(data, { reset }) {
+    const { email, password } = data;
+
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email('Insira um e-mail válido')
+          .required('O e-mail é obrigatório'),
+        password: Yup.string().required('A senha é obrigatória'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      formRef.current.setErrors({});
+
+      dispatch(signInRequest(email, password));
+
+      reset();
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errorMessages = {};
+
+        err.inner.forEach(error => {
+          errorMessages[error.path] = error.message;
+        });
+
+        formRef.current.setErrors(errorMessages);
+      }
+    }
+  }
+
+  return (
+    <Container>
+      <img src={logo} alt="FastFeet" />
+
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <Input
+          label="SEU E-MAIL"
+          name="email"
+          type="email"
+          placeholder="example@email.com"
+        />
+
+        <Input
+          label="SUA SENHA"
+          name="password"
+          type="password"
+          placeholder="********"
+        />
+
+        <Button type="submit">
+          {loading ? (
+            <BounceLoader size={25} color="#FFF" loading={loading} />
+          ) : (
+            'Entrar no sistema'
+          )}
+        </Button>
+      </Form>
+    </Container>
+  );
+}
---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #fff;
+
+  height: 350px;
+
+  border-radius: 5px;
+  padding: 10px;
+`;
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
