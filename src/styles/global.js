3c2039c40e1e38ab5b4ef85ae601affddd4cadac
Henrique Tavares <ihenrits@gmail.com>
2020-03-01 23:02:46 -0300

    chore: removing @import from the global style

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,9 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
   * {
     margin: 0;
     padding: 0;
