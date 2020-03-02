f8d1b0a67f3888aea2985bec6710a6b611c063b3
Henrique Tavares <ihenrits@gmail.com>
2020-03-01 23:00:33 -0300

    chore: add routes for new pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
-import Dashboard from '~/pages/Dashboard';
 import SignIn from '~/pages/SignIn';
+import Orders from '~/pages/Orders';
+import Deliverymen from '~/pages/Deliverymen';
+import Recipients from '~/pages/Recipients';
+import Problems from '~/pages/Problems';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
 
-      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/orders" component={Orders} isPrivate />
+      <Route path="/deliverymen" component={Deliverymen} isPrivate />
+      <Route path="/recipients" component={Recipients} isPrivate />
+      <Route path="/problems" component={Problems} isPrivate />
     </Switch>
   );
 }
