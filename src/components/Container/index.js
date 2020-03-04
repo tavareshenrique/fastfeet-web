63827b89860fd096450616f7141d0b25e1335e2d
Henrique Tavares <ihenrits@gmail.com>
2020-03-04 00:42:47 -0300

    chore: add Title and Search in Container

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Content } from './styles';
+import { FaPlus } from 'react-icons/fa';
 
-export default function Container({ children }) {
-  return <Content>{children}</Content>;
+import SearchBar from '~/components/SearchBar';
+
+import { Content, Title, HeaderBar } from './styles';
+
+export default function Container({ title, children }) {
+  return (
+    <Content>
+      <Title>{title}</Title>
+
+      <HeaderBar>
+        <SearchBar />
+        <button type="button">
+          <FaPlus color="#FFF" size={15} /> Cadastrar
+        </button>
+      </HeaderBar>
+      {children}
+    </Content>
+  );
 }
 
 Container.propTypes = {
   children: PropTypes.element.isRequired,
+  title: PropTypes.string.isRequired,
 };
--- src/components/Container/styles.js
+++ src/components/Container/styles.js
@@ -1,5 +1,34 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Content = styled.div`
   padding: 30px 120px 30px 120px;
+
+  button {
+    width: 150px;
+    margin: 5px 0 0;
+    height: 44px;
+    background: #816fe7;
+    font-weight: bold;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    font-size: 16px;
+    transition: background 0.2s;
+    &:hover {
+      background: ${darken(0.3, '#816fe7')};
+    }
+  }
+`;
+
+export const Title = styled.h2`
+  font-weight: bold;
+`;
+
+export const HeaderBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  padding-bottom: 15px;
 `;
