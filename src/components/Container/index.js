d1d659e9bca9a91dd01a614e9ba12b57163fb5a6
Henrique Tavares <ihenrits@gmail.com>
2020-03-12 12:17:38 -0300

    chore: add event onclick button Container

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -5,16 +5,16 @@ import { FaPlus } from 'react-icons/fa';
 
 import SearchBar from '~/components/SearchBar';
 
-import { Content, Title, HeaderBar } from './styles';
+import { Content, HeaderBar } from './styles';
 
-export default function Container({ title, children }) {
+export default function Container({ title, children, handleAdd }) {
   return (
     <Content>
-      <Title>{title}</Title>
+      <h2>{title}</h2>
 
       <HeaderBar>
         <SearchBar />
-        <button type="button">
+        <button type="button" onClick={handleAdd}>
           <FaPlus color="#FFF" size={15} /> Cadastrar
         </button>
       </HeaderBar>
@@ -26,4 +26,5 @@ export default function Container({ title, children }) {
 Container.propTypes = {
   children: PropTypes.element.isRequired,
   title: PropTypes.string.isRequired,
+  handleAdd: PropTypes.func.isRequired,
 };
