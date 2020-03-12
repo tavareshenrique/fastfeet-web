923989a482acbb25658d8414fb897da91d3998cc
Henrique Tavares <ihenrits@gmail.com>
2020-03-12 12:18:23 -0300

    chore: add container in input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { useField } from '@unform/core';
 
-import { LabelContainer, Error } from './styles';
+import { Container, LabelContainer, Error } from './styles';
 
 export default function Input({ name, label, ...rest }) {
   const inputRef = useRef(null);
@@ -17,7 +17,7 @@ export default function Input({ name, label, ...rest }) {
     });
   }, [fieldName, registerField]);
   return (
-    <>
+    <Container>
       <LabelContainer>
         {label && <label htmlFor={fieldName}>{label}</label>}
         {error && <Error>* {error}</Error>}
@@ -29,7 +29,7 @@ export default function Input({ name, label, ...rest }) {
         defaultValue={defaultValue}
         {...rest}
       />
-    </>
+    </Container>
   );
 }
 
--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin: 5px;
+`;
+
 export const LabelContainer = styled.div`
   display: flex;
 
