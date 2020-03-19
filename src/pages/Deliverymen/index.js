0d1fdf2d33a74ca1d01866650e691852fd2edf28
Henrique Tavares <ihenrits@gmail.com>
2020-03-19 19:43:42 -0300

    chore: add AvatarComponent in Deliverymen

--- src/pages/Deliverymen/index.js
+++ src/pages/Deliverymen/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
@@ -9,8 +9,9 @@ import Container from '~/components/Container';
 import Table from '~/components/Table';
 import Popover from '~/components/Popover';
 import Action from '~/components/Action';
+import Avatar from '~/components/Avatar';
 
-import { Photo, NoPhoto } from './styles';
+import { Photo } from './styles';
 
 export default function Deliverymen() {
   const history = useHistory();
@@ -20,77 +21,6 @@ export default function Deliverymen() {
 
   const searchWord = useSelector(state => state.search.searchWord);
   const dataRequest = useSelector(state => state.deliveryman.data);
-
-  const shortName = useCallback(name => {
-    let initials = name.match(/\b\w/g) || [];
-    initials = (
-      (initials.shift() || '') + (initials.pop() || '')
-    ).toUpperCase();
-    return initials;
-  }, []);
-
-  function randomNumber() {
-    const randomValue = Math.floor(Math.random() * 5) + 1;
-
-    return randomValue;
-  }
-
-  const columns = [
-    {
-      title: 'ID',
-      dataIndex: 'id',
-      key: 'id',
-      rowKey: 'id',
-    },
-    {
-      title: 'Foto',
-      dataIndex: 'foto',
-      key: 'foto',
-      rowKey: 'foto',
-      render: dataRender => {
-        if (!dataRender.nameAvatar) {
-          return <Photo src={dataRender.url} alt="Deliveryman" />;
-        }
-
-        return (
-          <NoPhoto color={randomNumber()}>
-            <span>{shortName(dataRender.nameAvatar)}</span>
-          </NoPhoto>
-        );
-      },
-    },
-    {
-      title: 'Nome',
-      dataIndex: 'name',
-      key: 'name',
-      rowKey: 'name',
-    },
-    {
-      title: 'Email',
-      dataIndex: 'email',
-      key: 'email',
-      rowKey: 'email',
-    },
-    {
-      title: 'Ação',
-      key: 'action',
-      width: 5,
-      rowKey: 'action',
-      render: dataRender => (
-        <div>
-          <Action onClick={() => setCurrentRow(dataRender.id)} />
-
-          <Popover
-            visible={currentRow === dataRender.id}
-            id={dataRender.id}
-            urlParam="deliverymen"
-            handleVisibleChange={visible => setCurrentRow(visible)}
-            showView={false}
-          />
-        </div>
-      ),
-    },
-  ];
 
   useEffect(() => {
     if (dataRequest.data) {
@@ -155,6 +85,59 @@ export default function Deliverymen() {
 
     fetchDeliverymen();
   }, []);
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+      rowKey: 'id',
+    },
+    {
+      title: 'Foto',
+      dataIndex: 'foto',
+      key: 'foto',
+      rowKey: 'foto',
+      render: dataRender => {
+        if (!dataRender.nameAvatar) {
+          return <Photo src={dataRender.url} alt="Deliveryman" />;
+        }
+
+        return <Avatar name={dataRender.nameAvatar} />;
+      },
+    },
+    {
+      title: 'Nome',
+      dataIndex: 'name',
+      key: 'name',
+      rowKey: 'name',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+      rowKey: 'email',
+    },
+    {
+      title: 'Ação',
+      key: 'action',
+      width: 5,
+      rowKey: 'action',
+      render: dataRender => (
+        <div>
+          <Action onClick={() => setCurrentRow(dataRender.id)} />
+
+          <Popover
+            visible={currentRow === dataRender.id}
+            id={dataRender.id}
+            urlParam="deliverymen"
+            handleVisibleChange={visible => setCurrentRow(visible)}
+            showView={false}
+          />
+        </div>
+      ),
+    },
+  ];
 
   return (
     <Container
