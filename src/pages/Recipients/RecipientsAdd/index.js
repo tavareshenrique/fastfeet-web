08cde6b59fe5abc1984ad27551bf16b1fb48074f
Henrique Tavares <ihenrits@gmail.com>
2020-03-19 23:53:51 -0300

    chore: find address by cep in RecipientAdd n' RecipientEdit

--- src/pages/Recipients/RecipientsAdd/index.js
+++ src/pages/Recipients/RecipientsAdd/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+
+import axios from 'axios';
 
 import { Row, Col } from 'antd';
 import { Form } from '@unform/web';
 import { FaCheck, FaChevronLeft } from 'react-icons/fa';
 import { BounceLoader } from 'react-spinners';
+import LoadingOverlay from 'react-loading-overlay';
 
 import { recipientPost } from '~/store/modules/recipient/actions';
 
@@ -26,107 +29,134 @@ export default function RecipientsAdd() {
 
   const loading = useSelector(state => state.order.loading);
 
+  const [dataAddress, setDataAddress] = useState([]);
+  const [loadingAddress, setLoadingAddress] = useState(false);
+
+  async function loadCep(cep) {
+    if (cep.length === 8) {
+      setLoadingAddress(true);
+      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+
+      setDataAddress({
+        street: response.data.logradouro,
+        zipcode: response.data.cep,
+        city: response.data.localidade,
+        state: response.data.uf,
+      });
+
+      setTimeout(() => {
+        setLoadingAddress(false);
+      }, 1200);
+    }
+  }
+
   function handleSubmit(data) {
     dispatch(recipientPost(data));
   }
 
   return (
-    <Container>
-      <Content>
-        <Form onSubmit={handleSubmit}>
-          <HeaderBar>
-            <h1>Cadastro de Destinatário</h1>
+    <LoadingOverlay active={loadingAddress} spinner text="Buscando endereço...">
+      <Container>
+        <Content>
+          <Form onSubmit={handleSubmit} initialData={dataAddress}>
+            <HeaderBar>
+              <h1>Cadastro de Destinatário</h1>
 
-            <ButtonContainer>
-              <button type="button" onClick={() => history.push('/recipients')}>
-                <FaChevronLeft color="#FFF" size={15} /> Voltar
-              </button>
+              <ButtonContainer>
+                <button
+                  type="button"
+                  onClick={() => history.push('/recipients')}
+                >
+                  <FaChevronLeft color="#FFF" size={15} /> Voltar
+                </button>
 
-              <button type="submit">
-                {loading ? (
-                  <ButtonText>
-                    <BounceLoader size={25} color="#FFF" loading={loading} />
-                  </ButtonText>
-                ) : (
-                  <ButtonText>
-                    <FaCheck color="#FFF" size={15} />
-                    Salvar
-                  </ButtonText>
-                )}
-              </button>
-            </ButtonContainer>
-          </HeaderBar>
+                <button type="submit">
+                  {loading ? (
+                    <ButtonText>
+                      <BounceLoader size={25} color="#FFF" loading={loading} />
+                    </ButtonText>
+                  ) : (
+                    <ButtonText>
+                      <FaCheck color="#FFF" size={15} />
+                      Salvar
+                    </ButtonText>
+                  )}
+                </button>
+              </ButtonContainer>
+            </HeaderBar>
 
-          <Row>
-            <Col span={24}>
-              <Input
-                label="Nome"
-                name="name"
-                type="text"
-                placeholder="Ex.: Henrique Tavares"
-              />
-            </Col>
-          </Row>
+            <Row>
+              <Col span={24}>
+                <Input
+                  label="Nome"
+                  name="name"
+                  type="text"
+                  placeholder="Ex.: Henrique Tavares"
+                />
+              </Col>
+            </Row>
 
-          <Row>
-            <Col span={12}>
-              <Input
-                label="Rua"
-                name="street"
-                type="text"
-                placeholder="Ex.: Rua Joaquim José da Silva Xavier"
-              />
-            </Col>
+            <Row>
+              <Col span={8}>
+                <Input
+                  label="CEP"
+                  name="zipcode"
+                  type="text"
+                  placeholder="Ex.: 00000000"
+                  onChange={e => loadCep(e.target.value)}
+                />
+              </Col>
 
-            <Col span={6}>
-              <Input
-                label="Número"
-                name="number"
-                type="text"
-                placeholder="Ex.: 100"
-              />
-            </Col>
+              <Col span={12}>
+                <Input
+                  label="Cidade"
+                  name="city"
+                  type="text"
+                  placeholder="Ex.: Rio de Janeiro"
+                />
+              </Col>
 
-            <Col span={6}>
-              <Input
-                label="Complemento"
-                name="complement"
-                type="text"
-                placeholder="Ex.: Casa 1"
-              />
-            </Col>
-          </Row>
+              <Col span={4}>
+                <Input
+                  label="UF"
+                  name="state"
+                  type="text"
+                  placeholder="Ex.: RJ"
+                />
+              </Col>
+            </Row>
 
-          <Row>
-            <Col span={8}>
-              <Input
-                label="CEP"
-                name="zipcode"
-                type="text"
-                placeholder="Ex.: 00000-000"
-              />
-            </Col>
+            <Row>
+              <Col span={12}>
+                <Input
+                  label="Rua"
+                  name="street"
+                  type="text"
+                  placeholder="Ex.: Rua Joaquim José da Silva Xavier"
+                />
+              </Col>
 
-            <Col span={12}>
-              <Input
-                label="Cidade"
-                name="city"
-                type="text"
-                placeholder="Ex.: Rio de Janeiro"
-              />
-            </Col>
+              <Col span={6}>
+                <Input
+                  label="Número"
+                  name="number"
+                  type="text"
+                  placeholder="Ex.: 100"
+                />
+              </Col>
 
-            <Col span={4}>
-              <Input
-                label="UF"
-                name="state"
-                type="text"
-                placeholder="Ex.: RJ"
-              />
-            </Col>
-          </Row>
-        </Form>
-      </Content>
-    </Container>
+              <Col span={6}>
+                <Input
+                  label="Complemento"
+                  name="complement"
+                  type="text"
+                  placeholder="Ex.: Casa 1"
+                />
+              </Col>
+            </Row>
+          </Form>
+        </Content>
+      </Container>
+    </LoadingOverlay>
   );
 }
--- src/pages/Recipients/RecipientsEdit/index.js
+++ src/pages/Recipients/RecipientsEdit/index.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
-
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
+
+import axios from 'axios';
 
 import { Row, Col } from 'antd';
 import { Form } from '@unform/web';
 import { FaCheck, FaChevronLeft } from 'react-icons/fa';
 import { BounceLoader } from 'react-spinners';
+import LoadingOverlay from 'react-loading-overlay';
 
 import { recipientUpdate } from '~/store/modules/recipient/actions';
 
@@ -30,6 +32,32 @@ export default function RecipientsAdd() {
   const loading = useSelector(state => state.order.loading);
 
   const [data, setData] = useState([]);
+
+  const [loadingAddress, setLoadingAddress] = useState(false);
+
+  async function loadCep(cep) {
+    if (cep.length === 8) {
+      setLoadingAddress(true);
+      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+
+      const { id: idRecipient, name } = data;
+
+      setData({
+        id: idRecipient,
+        name,
+        number: '',
+        complement: '',
+        street: response.data.logradouro,
+        zipcode: response.data.cep,
+        city: response.data.localidade,
+        state: response.data.uf,
+      });
+
+      setTimeout(() => {
+        setLoadingAddress(false);
+      }, 1200);
+    }
+  }
 
   useEffect(() => {
     async function fectRecipient() {
@@ -61,102 +89,108 @@ export default function RecipientsAdd() {
   }
 
   return (
-    <Container>
-      <Content>
-        <Form onSubmit={handleSubmit} initialData={data}>
-          <HeaderBar>
-            <h1>Alteração de Entregador</h1>
+    <LoadingOverlay active={loadingAddress} spinner text="Buscando endereço...">
+      <Container>
+        <Content>
+          <Form onSubmit={handleSubmit} initialData={data}>
+            <HeaderBar>
+              <h1>Alteração de Entregador</h1>
 
-            <ButtonContainer>
-              <button type="button" onClick={() => history.push('/recipients')}>
-                <FaChevronLeft color="#FFF" size={15} /> Voltar
-              </button>
+              <ButtonContainer>
+                <button
+                  type="button"
+                  onClick={() => history.push('/recipients')}
+                >
+                  <FaChevronLeft color="#FFF" size={15} /> Voltar
+                </button>
 
-              <button type="submit">
-                {loading ? (
-                  <ButtonText>
-                    <BounceLoader size={25} color="#FFF" loading={loading} />
-                  </ButtonText>
-                ) : (
-                  <ButtonText>
-                    <FaCheck color="#FFF" size={15} />
-                    Salvar
-                  </ButtonText>
-                )}
-              </button>
-            </ButtonContainer>
-          </HeaderBar>
+                <button type="submit">
+                  {loading ? (
+                    <ButtonText>
+                      <BounceLoader size={25} color="#FFF" loading={loading} />
+                    </ButtonText>
+                  ) : (
+                    <ButtonText>
+                      <FaCheck color="#FFF" size={15} />
+                      Salvar
+                    </ButtonText>
+                  )}
+                </button>
+              </ButtonContainer>
+            </HeaderBar>
 
-          <Row>
-            <Col span={24}>
-              <Input
-                label="Nome"
-                name="name"
-                type="text"
-                placeholder="Ex.: Henrique Tavares"
-              />
-            </Col>
-          </Row>
+            <Row>
+              <Col span={24}>
+                <Input
+                  label="Nome"
+                  name="name"
+                  type="text"
+                  placeholder="Ex.: Henrique Tavares"
+                />
+              </Col>
+            </Row>
 
-          <Row>
-            <Col span={12}>
-              <Input
-                label="Rua"
-                name="street"
-                type="text"
-                placeholder="Ex.: Rua Joaquim José da Silva Xavier"
-              />
-            </Col>
+            <Row>
+              <Col span={8}>
+                <Input
+                  label="CEP"
+                  name="zipcode"
+                  type="text"
+                  placeholder="Ex.: 00000000"
+                  onChange={e => loadCep(e.target.value)}
+                />
+              </Col>
 
-            <Col span={6}>
-              <Input
-                label="Número"
-                name="number"
-                type="text"
-                placeholder="Ex.: 100"
-              />
-            </Col>
+              <Col span={12}>
+                <Input
+                  label="Cidade"
+                  name="city"
+                  type="text"
+                  placeholder="Ex.: Rio de Janeiro"
+                />
+              </Col>
 
-            <Col span={6}>
-              <Input
-                label="Complemento"
-                name="complement"
-                type="text"
-                placeholder="Ex.: Casa 1"
-              />
-            </Col>
-          </Row>
+              <Col span={4}>
+                <Input
+                  label="UF"
+                  name="state"
+                  type="text"
+                  placeholder="Ex.: RJ"
+                />
+              </Col>
+            </Row>
 
-          <Row>
-            <Col span={8}>
-              <Input
-                label="CEP"
-                name="zipcode"
-                type="text"
-                placeholder="Ex.: 00000-000"
-              />
-            </Col>
+            <Row>
+              <Col span={12}>
+                <Input
+                  label="Rua"
+                  name="street"
+                  type="text"
+                  placeholder="Ex.: Rua Joaquim José da Silva Xavier"
+                />
+              </Col>
 
-            <Col span={12}>
-              <Input
-                label="Cidade"
-                name="city"
-                type="text"
-                placeholder="Ex.: Rio de Janeiro"
-              />
-            </Col>
+              <Col span={6}>
+                <Input
+                  label="Número"
+                  name="number"
+                  type="text"
+                  placeholder="Ex.: 100"
+                />
+              </Col>
 
-            <Col span={4}>
-              <Input
-                label="UF"
-                name="state"
-                type="text"
-                placeholder="Ex.: RJ"
-              />
-            </Col>
-          </Row>
-        </Form>
-      </Content>
-    </Container>
+              <Col span={6}>
+                <Input
+                  label="Complemento"
+                  name="complement"
+                  type="text"
+                  placeholder="Ex.: Casa 1"
+                />
+              </Col>
+            </Row>
+          </Form>
+        </Content>
+      </Container>
+    </LoadingOverlay>
   );
 }
